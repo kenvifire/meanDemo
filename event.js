883e74da3600b4b913da2d6d2463ebcd460b4d41
kenvi <mrwhite@163.com>
2015-06-25 23:52:39 +0800

    event.js

---
+++ event.js
@@ -0,0 +1,20 @@
+var util = require('util');
+
+var EventEmitter = require('events').EventEmitter;
+
+function Counter() {
+	var self = this;
+
+	EventEmitter.call(this);
+
+	var count = 0;
+
+	this.start = function() {
+		this.emit('start');
+
+		setInterval(function() {
+			self.emit('count', count);
+			++count;
+		},1000);
+	}
+}
